17d6cea95b6149b71c2af066dd94101a77aaa156
mlnawsdevops <dev@123.email>
2026-03-19 07:09:52 +0530

    main

--- print-error-logs.sh
+++ print-error-logs.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 curl https://raw.githubusercontent.com/iam-veeramalla/sandbox/refs/heads/main/log/dummylog01122022.log | grep TRACE > error.log
+
+# curl <file-path> | grep <word-to-find>  > <copying-output-in-file>
