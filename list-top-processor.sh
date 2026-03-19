9e0f7aa82bb6d88a66c422595d41985360a672f9
mlnawsdevops <dev@123.email>
2026-03-19 06:34:36 +0530

    main

--- list-top-processor.sh
+++ list-top-processor.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# -e is used to display all running processes in the system, across all users.
+# -o is used to customize the output format, allowing us to specify which columns or fields we want to display.
+
 ps -eo pid,ppid,cmd,%cpu --sort=-%cpu | head
+top
